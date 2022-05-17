f62e8063be2b1d074c28258848447123fb901496
Marijn Haverbeke <marijn@haverbeke.nl>
2022-05-17 14:40:36 +0200

    Port to TypeScript

--- src/inputrules.ts
+++ src/inputrules.ts
@@ -1,33 +1,41 @@
-import {Plugin} from "prosemirror-state"
+import {Plugin, Transaction, EditorState, TextSelection, Command} from "prosemirror-state"
+import {EditorView} from "prosemirror-view"
 
-// ::- Input rules are regular expressions describing a piece of text
-// that, when typed, causes something to happen. This might be
-// changing two dashes into an emdash, wrapping a paragraph starting
-// with `"> "` into a blockquote, or something entirely different.
+/// Input rules are regular expressions describing a piece of text
+/// that, when typed, causes something to happen. This might be
+/// changing two dashes into an emdash, wrapping a paragraph starting
+/// with `"> "` into a blockquote, or something entirely different.
 export class InputRule {
+  /// @internal
+  handler: (state: EditorState, match: RegExpMatchArray, start: number, end: number) => Transaction | null
+
   // :: (RegExp, union<string, (state: EditorState, match: [string], start: number, end: number) → ?Transaction>)
-  // Create an input rule. The rule applies when the user typed
-  // something and the text directly in front of the cursor matches
-  // `match`, which should end with `$`.
-  //
-  // The `handler` can be a string, in which case the matched text, or
-  // the first matched group in the regexp, is replaced by that
-  // string.
-  //
-  // Or a it can be a function, which will be called with the match
-  // array produced by
-  // [`RegExp.exec`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/exec),
-  // as well as the start and end of the matched range, and which can
-  // return a [transaction](#state.Transaction) that describes the
-  // rule's effect, or null to indicate the input was not handled.
-  constructor(match, handler) {
+  /// Create an input rule. The rule applies when the user typed
+  /// something and the text directly in front of the cursor matches
+  /// `match`, which should end with `$`.
+  ///
+  /// The `handler` can be a string, in which case the matched text, or
+  /// the first matched group in the regexp, is replaced by that
+  /// string.
+  ///
+  /// Or a it can be a function, which will be called with the match
+  /// array produced by
+  /// [`RegExp.exec`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/exec),
+  /// as well as the start and end of the matched range, and which can
+  /// return a [transaction](#state.Transaction) that describes the
+  /// rule's effect, or null to indicate the input was not handled.
+  constructor(
+    /// @internal
+    readonly match: RegExp,
+    handler: string | ((state: EditorState, match: RegExpMatchArray, start: number, end: number) => Transaction | null)
+  ) {
     this.match = match
     this.handler = typeof handler == "string" ? stringHandler(handler) : handler
   }
 }
 
-function stringHandler(string) {
-  return function(state, match, start, end) {
+function stringHandler(string: string) {
+  return function(state: EditorState, match: RegExpMatchArray, start: number, end: number) {
     let insert = string
     if (match[1]) {
       let offset = match[0].lastIndexOf(match[1])
@@ -45,15 +53,14 @@ function stringHandler(string) {
 
 const MAX_MATCH = 500
 
-// :: (config: {rules: [InputRule]}) → Plugin
-// Create an input rules plugin. When enabled, it will cause text
-// input that matches any of the given rules to trigger the rule's
-// action.
-export function inputRules({rules}) {
-  let plugin = new Plugin({
+/// Create an input rules plugin. When enabled, it will cause text
+/// input that matches any of the given rules to trigger the rule's
+/// action.
+export function inputRules({rules}: {rules: readonly InputRule[]}) {
+  let plugin: Plugin = new Plugin({
     state: {
       init() { return null },
-      apply(tr, prev) {
+      apply(this: Plugin, tr, prev) {
         let stored = tr.getMeta(this)
         if (stored) return stored
         return tr.selectionSet || tr.docChanged ? null : prev
@@ -67,19 +74,20 @@ export function inputRules({rules}) {
       handleDOMEvents: {
         compositionend: (view) => {
           setTimeout(() => {
-            let {$cursor} = view.state.selection
+            let {$cursor} = view.state.selection as TextSelection
             if ($cursor) run(view, $cursor.pos, $cursor.pos, "", rules, plugin)
           })
         }
       }
     },
 
+    // @ts-ignore
     isInputRules: true
   })
   return plugin
 }
 
-function run(view, from, to, text, rules, plugin) {
+function run(view: EditorView, from: number, to: number, text: string, rules: readonly InputRule[], plugin: Plugin) {
   if (view.composing) return false
   let state = view.state, $from = state.doc.resolve(from)
   if ($from.parent.type.spec.code) return false
@@ -95,14 +103,13 @@ function run(view, from, to, text, rules, plugin) {
   return false
 }
 
-// :: (EditorState, ?(Transaction)) → bool
-// This is a command that will undo an input rule, if applying such a
-// rule was the last thing that the user did.
-export function undoInputRule(state, dispatch) {
+/// This is a command that will undo an input rule, if applying such a
+/// rule was the last thing that the user did.
+export const undoInputRule: Command = (state, dispatch) => {
   let plugins = state.plugins
   for (let i = 0; i < plugins.length; i++) {
     let plugin = plugins[i], undoable
-    if (plugin.spec.isInputRules && (undoable = plugin.getState(state))) {
+    if ((plugin.spec as any).isInputRules && (undoable = plugin.getState(state))) {
       if (dispatch) {
         let tr = state.tr, toUndo = undoable.transform
         for (let j = toUndo.steps.length - 1; j >= 0; j--)
